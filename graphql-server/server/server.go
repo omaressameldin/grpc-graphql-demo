6b3a1ae9aeb1605635a7ffc1bad39a1eedc402dd
Omar Essam <o.essameldin@gmail.com>
2019-03-05 15:55:01 +0100

    add newGraphQLResolver to connect to grpc service just once

--- graphql-server/server/server.go
+++ graphql-server/server/server.go
@@ -4,12 +4,31 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"google.golang.org/grpc"
 
 	"github.com/99designs/gqlgen/handler"
 	"github.com/omaressameldin/grpc-graphql-demo/graphql-server"
 )
 
+
+
+func newGraphQLResolver() (*graphql_server.Resolver, error) {
+	todoClient, err := connectToService()
+	if err != nil {
+		return nil, err
+	}
+
+	return &graphql_server.Resolver{
+		TodoClient: todoClient,
+	}, nil
+}
+
+
 const defaultPort = "8080"
+
+func connectToService() (*grpc.ClientConn, error) {
+	return grpc.Dial("grpc-server:3000", grpc.WithInsecure())
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -17,9 +36,17 @@ func main() {
 		port = defaultPort
 	}
 
+	resolver, err := newGraphQLResolver()
+	if err != nil {
+		log.Fatalf("couldnt connect to server: %v", err)
+	}
+
 	http.Handle("/", handler.Playground("GraphQL playground", "/query"))
-	http.Handle("/query", handler.GraphQL(graphql_server.NewExecutableSchema(graphql_server.Config{Resolvers: &graphql_server.Resolver{}})))
+	http.Handle("/query", handler.GraphQL(graphql_server.NewExecutableSchema(graphql_server.Config{Resolvers: resolver})))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+
+
+	defer resolver.TodoClient.Close()
 }
